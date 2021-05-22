86258fc690898ce9c5c0cb9c94ebe9e057842c31
jumpmelon <lijiaheng09@outlook.com>
2021-05-22 14:54:37 +0800

    upd - interface

--- main.h
+++ main.h
@@ -8,6 +8,7 @@ int getBidValue(int maxBid); // 叫分
 CardCombo getAction(); // 出牌, 搜索记得维护全局变量
 
 int search(); // 最优得分
+std::vector<CardCombo> candidates();
 std::vector<std::pair<CardDistrib, double>> randCards(int num);
 double evalCards(const CardSet &); // 期望得分
 double eval(const CardCombo &); // 期望得分
