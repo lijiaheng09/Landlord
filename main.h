4b2649c19f5ad46443bbd549e73c7fefd752bd1e
jumpmelon <lijiaheng09@outlook.com>
2021-05-22 14:26:45 +0800

    upd - interface

--- main.h
+++ main.h
@@ -9,6 +9,7 @@ CardCombo getAction(); // 出牌, 搜索记得维护全局变量
 
 bool search();
 std::vector<std::pair<CardDistrib, double>> randCards(int num);
+double evalCards(const CardSet &);
 double eval(const CardCombo &);
 
 #endif
