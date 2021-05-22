22c91f1a41356d1971e3728f7009b5ba47b0a74f
jumpmelon <lijiaheng09@outlook.com>
2021-05-22 17:54:41 +0800

    upd main.h

--- main.h
+++ main.h
@@ -11,6 +11,7 @@ double getMean(); // 根据当前牌得到最优策略得分期望
 int search(); // 最优得分
 std::vector<CardCombo> getCandidates();
 std::vector<std::pair<CardDistrib, double>> randCards(int num);
+vector<pair<double, CardCombo>> getCandidatesEval(int num);
 double evalCards(const CardSet &); // 期望得分
 double eval(const CardCombo &); // 期望得分
 
